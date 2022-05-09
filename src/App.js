86a7d529e455166446f68966f527bdb99a361018
Bohdan Yaroshchuk <49143627+OscarVegener@users.noreply.github.com>
2022-05-09 15:15:04 +0300

    Removed console logs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import Search from "./components/search/search.component";
 import CardList from "./components/cardList/cardList.component";
 
 const App = () => {
-  console.log("render");
 
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
-    console.log("use effect 1");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setMonsters(users)); 
   }, []);
 
   useEffect(() => {
-    console.log("use effect filter");
     setFilteredMonsters(
       monsters.filter((monster) => {
         return monster.name.toLocaleLowerCase().includes(searchField);
